1f93c26e03f8af5523fd821f4eb67010d96b3b08
Mark Palencia <111488348+DwayneDantes@users.noreply.github.com>
2023-01-27 10:54:52 +0800

    Create Myself.java

---
+++ exercise 4/Myself.java
@@ -0,0 +1,21 @@
+public class Myself {
+    
+    int age;
+    String name;
+    
+    //Constructor 
+    
+    public Myself(String n, int a){
+    
+        this.age=a;
+        this.name=n;
+    }
+    
+    public void AddFriend(Person friend){
+            System.out.println("I am friends with " + friend.name);
+
+    }
+    }
+    
+
+
