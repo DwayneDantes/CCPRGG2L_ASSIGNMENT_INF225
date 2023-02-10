8affe93ee70b6fbd645b3e822040d4db67153d50
Mark Palencia <111488348+DwayneDantes@users.noreply.github.com>
2023-02-10 10:06:57 +0800

    Create Daddy.java

---
+++ Exercise 5/Daddy.java
@@ -0,0 +1,15 @@
+public class Daddy {
+    
+    protected static String surname = "Palencia";
+
+    //Methods
+    public String getName() {
+        return this.surname;
+    }
+
+    //showlove
+    public void showLove(){
+        System.out.println("I love my " + surname + " family ");
+    }
+}
+
