0400c1a33743645890950d82311c3c62cae98893
Mark Palencia <111488348+DwayneDantes@users.noreply.github.com>
2023-02-10 10:06:43 +0800

    Create Number.java

---
+++ Exercise 5/Number.java
@@ -0,0 +1,18 @@
+public class Number {
+    private int CCnumber;
+
+
+
+
+    //get method 
+
+    public int getNumber(){
+        return this.CCnumber;
+    }
+
+    //set method
+    public void setNumber(int newNumber){
+        this.CCnumber = newNumber;
+    }
+}
+    
